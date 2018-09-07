f496b303b1b5dadfe01c50e97e6bb00e723d8f85
ShanaMaid <uestczeng@gmail>
2018-09-07 15:23:14 +0800

    Fri Sep 07 2018 15:23:14 GMT+0800 (CST)

--- up.js
+++ up.js
@@ -1,4 +1,6 @@
 const {execSync} = require('child_process');
+execSync('git config user.name "ShanaMaid"');
+execSync('git config user.email "uestczeng@gmail"');
 execSync('git add -A');
 execSync(`git commit -m "${new Date()}"`);
 execSync('git push');
