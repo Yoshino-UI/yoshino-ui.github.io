c68ad564f0edd302db8fcfdd69e8ebb87e590ca1
ShanaMaid <uestczeng@gmail.com>
2019-01-29 14:12:15 +0800

    Tue Jan 29 2019 14:12:15 GMT+0800 (CST)

--- js/yoshino-icon.js
+++ js/yoshino-icon.js
@@ -26,7 +26,7 @@
 /*!*********************************************!*\
   !*** ./docs/pages/components/Icon/index.md ***!
   \*********************************************/
-/*! no static exports found */function(module,exports){eval('module.exports = "## Icon 图标\\n展示对应的矢量化图标。\\n\\nIcon组件的图标库采用了`Ionicons 4.4.2`，对图标的导入采用按需加载，你无须担心仅使用了一个字体就引入了所有图标文件。\\n\\n如果不相信你可以按`F12`打开控制台，然后再该页面刷新，查看`network`的图标加载情况。\\n  \\n## 代码演示\\n"\n\n//# sourceURL=webpack:///./docs/pages/components/Icon/index.md?')},"./docs/pages/components/Icon/index.tsx":
+/*! no static exports found */function(module,exports){eval('module.exports = "## Icon 图标\\n展示对应的矢量化图标。\\n\\nIcon组件的图标库采用了`Ionicons 4.4.2`，对图标的导入采用按需加载，你无须担心仅使用了一个字体就引入了所有图标文件。\\n\\n如果不相信你可以按`F12`打开控制台，然后再该页面刷新，查看`network`的图标加载情况。\\n\\n为了减少项目中的svg的http请求，使用了`Archer`异步缓存本地svg。\\n\\n`Archer`全局唯一示例，修改配置请使用Icon.Archer.xxx调用对应方法，[Archerw文档地址](https://github.com/ShanaMaid/archer-svgs)\\n  \\n## 代码演示\\n"\n\n//# sourceURL=webpack:///./docs/pages/components/Icon/index.md?')},"./docs/pages/components/Icon/index.tsx":
 /*!**********************************************!*\
   !*** ./docs/pages/components/Icon/index.tsx ***!
   \**********************************************/
